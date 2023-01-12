37f95c034bd0e087aa5e400f1ed1dbde091c8c48
Rishabh Sharma <itsrishabhsharma@gmail.comm>
2023-01-12 14:42:51 +0530

    UI developed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./MyComponents/Header";
+import { Todos } from "./MyComponents/Todos";
+import { Footer } from "./MyComponents/Footer";
+import { AddTodo } from "./MyComponents/AddTodo";
+import React, { useState } from 'react';
+
 
 function App() {
+
+  const onDelete = (todo) => {
+    console.log("i am onDelete of todo", todo);
+    //Deleting this way in React does not work
+    // let index = todos.indexOf(todo);
+    // todos.splice(index, 1);
+
+    setTodos(todos.filter((e) => {
+      return e !== todo;
+    }))
+  }
+
+  const [todos, setTodos] = useState([
+    {
+      sno: 1,
+      title: "Go to the market",
+      desc: "You need to fo to the market to get this job done"
+    },
+    {
+      sno: 2,
+      title: "Go to the mall",
+      desc: "You need to fo to the market to get this job done2"
+    },
+    {
+      sno: 3,
+      title: "Go to the gym",
+      desc: "You need to fo to the market to get this job done3"
+    }
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header title="My Todos List" searchBar={false} />
+      <AddTodo />
+      <Todos todos={todos} onDelete={onDelete} />
+      <Footer />
+    </>
   );
 }
 
